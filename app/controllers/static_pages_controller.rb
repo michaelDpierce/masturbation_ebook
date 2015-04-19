139ebb079e381e48ec64d20b935f967db817ac63
Michael Pierce <michael@affektive.com>
2015-04-19 14:32:01 -0600

    First Commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
-  def home
+  def index
   end
-
-  def about
+  def quotes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Michaeldeanpierce::Application.routes.draw do
   resources :contacts
   match 'contact', to: 'contacts#new', via: 'get'
-  root 'static_pages#work'
-  match '/', to: 'static_pages#work', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
+  root 'static_pages#index'
+  match '/', to: 'static_pages#index', via: 'get'
+  match '/the_truth_about_masturbation_quotes', to: 'static_pages#quotes', via: 'get'
 end
