5aacc88b309815f1b08b639ca512acc3a4f12c83
Michael Pierce <michael@affektive.com>
2015-04-19 15:05:02 -0600

    Cleaning up css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Michaeldeanpierce::Application.routes.draw do
   resources :contacts
-  match 'contact', to: 'contacts#new', via: 'get'
+  match 'the_truth_about_masturbation_send_me_my_free_chapter', to: 'contacts#new', via: 'get'
   root 'static_pages#index'
   match '/', to: 'static_pages#index', via: 'get'
   match '/the_truth_about_masturbation_quotes', to: 'static_pages#quotes', via: 'get'
