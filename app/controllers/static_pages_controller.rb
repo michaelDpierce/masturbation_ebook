cd885e7195a88c19f6882e37c2b26b0a87c635e2
Michael Pierce <michael@affektive.com>
2015-04-21 20:09:27 -0600

    Basic blog added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,12 @@ class StaticPagesController < ApplicationController
   end
   def quotes
   end
+  def blog
+  end
+  def article_one
+  end
+  def article_two
+  end
+  def article_three
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Michaeldeanpierce::Application.routes.draw do
   match 'the_truth_about_masturbation_send_me_my_free_chapter', to: 'contacts#new', via: 'get'
   root 'static_pages#index'
   match '/', to: 'static_pages#index', via: 'get'
+  match '/blog', to: 'static_pages#blog', via: 'get'
+  match '/corn_flakes_and_masturbation', to: 'static_pages#article_one', via: 'get'
+  match '/monkeys_enjoy_porn', to: 'static_pages#article_two', via: 'get'
+  match '/is_masturbation_a_sin', to: 'static_pages#article_three', via: 'get'
   match '/the_truth_about_masturbation_quotes', to: 'static_pages#quotes', via: 'get'
 end
